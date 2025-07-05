f1ad9194f506942c168c918bee14b6e188854f14
Ryan5473 <rayenbelkahla219@gmail.com>
2025-07-05 20:06:46 +0100

    5jul

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -11,35 +11,12 @@ export const projects: Project[] = [
 }
 ,
   {
-    title: 'Team Management',
+    title: 'DoctorManagment',
     description: 'A collaborative task management application with real-time updates.',
     technologies: ['React', 'Node.js', 'MongoDB', 'Socket.io'],
     githubLink: 'https://github.com',
     videoId: 'eo17hz0IVDo',
     image: '/projects/task-manager.webp',
   },
-  {
-    title: 'Library Management',
-    description: 'My personal portfolio website showcasing my projects and skills.',
-    technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
-    githubLink: 'https://github.com',
-    videoId: 'eo17hz0IVDo',
-    image: '/projects/portfolio-website.jpg',
-  },
-  {
-    title: 'School Management',
-    description: 'A blogging platform with user authentication and markdown support.',
-    technologies: ['Gatsby', 'GraphQL', 'Contentful'],
-    githubLink: 'https://github.com',
-    videoId: 'eo17hz0IVDo',
-    image: '/projects/blog-website.jpeg',
-  },
-  {
-    title: 'Doctor Appointment',
-    description: 'A weather application that provides real-time weather updates.',
-    technologies: ['React', 'OpenWeatherMap API'],
-    githubLink: 'https://github.com',
-    videoId: 'eo17hz0IVDo',
-    image: '/projects/weather-app.png',
-  },
+  
 ];
